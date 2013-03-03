f5e08a95358008bf4fe378bed2fc9276800f7af5
Alexander Ramirez <adr2370@gmail.com>
2013-03-02 18:49:07 -0800

    Added fb to firebase javascript file

--- js/fireFunctions.js
+++ js/fireFunctions.js
@@ -1,46 +1,47 @@
-function addNode(fb,name) {
+var fb = new Firebase("https://skillpath.firebaseio.com/");
+function addNode(name) {
 	return fb.child("node").push({name: name}).name();
 }
-function removeNode(fb,node) {
+function removeNode(node) {
 	fb.child("node").child(node).set(null);
 }
-function addConnection(fb,tree,parent,child) {
+function addConnection(tree,parent,child) {
 	fb.child("tree").child(tree).child("nodes").child(parent).child("children").child(child).set(1);
 	fb.child("tree").child(tree).child("nodes").child(child).child("parents").child(parent).set(1);
-	updateNodeLevel(fb,tree,child);
+	updateNodeLevel(tree,child);
 }
-function removeConnection(fb,tree,parent,child) {
+function removeConnection(tree,parent,child) {
 	fb.child("tree").child(tree).child("nodes").child(parent).child("children").child(child).set(null);
 	fb.child("tree").child(tree).child("nodes").child(child).child("parents").child(parent).set(null);
-	updateNodeLevel(fb,tree,child);
+	updateNodeLevel(tree,child);
 }
-function addLink(fb,node,url) {
+function addLink(node,url) {
 	return fb.child("node").child(node).child("links").child(url).set({down: 0, up: 0}).name();
 }
-function removeLink(fb,node,url) {
+function removeLink(node,url) {
 	fb.child("node").child(node).child("links").child(url).set(null);
 }
-function addDownVote(fb,node,link) {
+function addDownVote(node,link) {
 	fb.child("node").child(node).child("links").child(link).child("down").once("value", function(dataSnapshot) {
 		fb.child("node").child(node).child("links").child(link).child("down").set(dataSnapshot.val()+1);
 	});
 }
-function addUpVote(fb,node,link) {
+function addUpVote(node,link) {
 	fb.child("node").child(node).child("links").child(link).child("up").once("value", function(dataSnapshot) {
 		fb.child("node").child(node).child("links").child(link).child("up").set(dataSnapshot.val()+1);
 	});
 }
-function addTree(fb,name,type) {
+function addTree(name,type) {
 	return fb.child("tree").push({name: name, type: type}).name();
 }
-function removeTree(fb,tree) {
+function removeTree(tree) {
 	fb.child("tree").child(tree).set(null);
 }
-function addNodeToTree(fb,tree,node) {
+function addNodeToTree(tree,node) {
 	fb.child("tree").child(tree).child("nodes").child(node).child("level").set(0);
-	changeNodeLevel(fb,tree,node,0);
+	changeNodeLevel(tree,node,0);
 }
-function updateNodeLevel(fb,tree,node) {
+function updateNodeLevel(tree,node) {
 	fb.child("tree").child(tree).child("nodes").child(node).once("value", function(nodeSnapshot) {
 		fb.child("tree").child(tree).once("value", function(snap) {
 			var parents=nodeSnapshot.child("parents");
@@ -55,46 +56,46 @@ function updateNodeLevel(fb,tree,node) {
 					}
 				}
 			});
-			changeNodeLevel(fb,tree,node,maxLevel);
+			changeNodeLevel(tree,node,maxLevel);
 			children.forEach(function(c) {
 				if(nodes.child(c.name()).val()!=null) {
 					var curr=nodes.child(c.name()).child("level").val();
 					if(curr<maxLevel+1) {
-						changeNodeLevel(fb,tree,c.name(),maxLevel+1);
+						changeNodeLevel(tree,c.name(),maxLevel+1);
 					}
 				}
 			});
 		});
 	});
 }
-function changeNodeLevel(fb,tree,node,newLevel) {
+function changeNodeLevel(tree,node,newLevel) {
 	fb.child("tree").child(tree).child("nodes").child(node).child("level").once("value", function(dataSnapshot) {
 		fb.child("tree").child(tree).child("levels").child(dataSnapshot.val()).child(node).set(null);
 		fb.child("tree").child(tree).child("levels").child(newLevel).child(node).set(1);
 		fb.child("tree").child(tree).child("nodes").child(node).child("level").set(newLevel);
 	});
 }
-function addUser(fb,userid,name,photo,email) {
+function addUser(userid,name,photo,email) {
 	fb.child("user").child(userid).set({name: name, photo: photo, email: email});
 }
-function removeUser(fb,userid) {
+function removeUser(userid) {
 	fb.child("user").child(userid).set(null);
 }
-function addUserCategory(fb,userid,category) {
+function addUserCategory(userid,category) {
 	fb.child("user").child(userid).child("categories").child(category).set(1);
 }
-function addCompletedNode(fb,userid,node) {
+function addCompletedNode(userid,node) {
 	fb.child("user").child(userid).child("completed").child(node).set(1);
 }
-function addGoal(fb,userid,node) {
+function addGoal(userid,node) {
 	fb.child("user").child(userid).child("goals").child(node).set(1);
 }
-function removeUserCategory(fb,userid,category) {
+function removeUserCategory(userid,category) {
 	fb.child("user").child(userid).child("categories").child(category).set(null);
 }
-function removeCompletedNode(fb,userid,node) {
+function removeCompletedNode(userid,node) {
 	fb.child("user").child(userid).child("completed").child(node).set(null);
 }
-function removeGoal(fb,userid,node) {
+function removeGoal(userid,node) {
 	fb.child("user").child(userid).child("goals").child(node).set(null);
 }
